e9ddca2aed19f3b8e9abbfcff136acb9d4e0322b
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-04-26 11:55:40 -0300

    feat: add PostgreSQL database connection setup in connection module

--- app/database/connection.py
+++ app/database/connection.py
@@ -3,3 +3,7 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
