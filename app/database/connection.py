d3fc1596d8af4575ec02fddcb4988b5dce1c27ab
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-04-28 10:10:35 -0300

    chore(database): add Base import and dotenv loading in connection module

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-from app.core.dependencies import Base
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+Base = declarative_base()
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base.metadata.create_all(bind=engine)
+def init_db():
+    Base.metadata.create_all(bind=engine)
