955fba911d4a329d60d276554791b78870eb115d
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-04-28 10:15:43 -0300

    refactor(repository): add event fields comparison in save method

--- app/repositories/event_repository.py
+++ app/repositories/event_repository.py
@@ -1,4 +1,4 @@
-from models.agenda import Event
+from app.models.agenda import Event
 from sqlalchemy.orm import Session
 
 
@@ -7,14 +7,19 @@ class EventRepository:
         self.session = session
 
     def save(self, events):
+        
+        event_fields = {'datetime', 'href', 'location', 'start', 'title'}
+        
         for event in events:
-            
             exists = self.session.query(Event).filter_by(
-                href=event["href"]
+                href=event.get("href")
             ).first()
 
             if not exists:
-                new_event = Event(**event)
+                
+                event_data = {k: v for k, v in event.items() if k in event_fields}
+                new_event = Event(**event_data)
                 self.session.add(new_event)
 
         self.session.commit()
+        
