07d55cd9fc2b83021c5531ab1721d9ce1f28b71d
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-04-30 10:24:33 -0300

    feat: integrate authority persistence into event repository

--- app/repositories/event_repository.py
+++ app/repositories/event_repository.py
@@ -1,25 +1,29 @@
 from app.models.agenda import Event
+from app.repositories.authority_repository import AuthorityRepository
 from sqlalchemy.orm import Session
 
 
 class EventRepository:
     def __init__(self, session: Session):
         self.session = session
+        self.authority_repository = AuthorityRepository(session)
 
     def save(self, events):
-        
         event_fields = {'datetime', 'href', 'location', 'start', 'title'}
-        
+
         for event in events:
             exists = self.session.query(Event).filter_by(
                 href=event.get("href")
             ).first()
 
             if not exists:
-                
                 event_data = {k: v for k, v in event.items() if k in event_fields}
                 new_event = Event(**event_data)
                 self.session.add(new_event)
+                self.session.flush()
+                self.authority_repository.save_for_event(new_event, event.get("href"))
+            else:
+                self.authority_repository.save_for_event(exists, event.get("href"))
 
         self.session.commit()
         
