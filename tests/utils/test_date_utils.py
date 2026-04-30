8025eaf56002976a53a83a626f6231c8275faa42
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-04-30 17:36:08 -0300

    test(dates): add unit tests for generate_dates

--- tests/utils/test_date_utils.py
+++ tests/utils/test_date_utils.py
@@ -1,2 +1,19 @@
-def test_generate_dates():
-    pass
+from app.utils.date_utils import generate_dates
+
+
+class TestGenerateDates:
+    
+    
+    def test_generate_dates(self):
+        assert generate_dates("2026-01-04", "2026-01-06") == [
+            '2026-01-04',
+            '2026-01-05',
+            '2026-01-06'
+        ]
+        
+   
+    def test_single_day(self):
+        assert generate_dates("2026-01-04", "2026-01-04") == [
+            "2026-01-04"
+        ]
+            
