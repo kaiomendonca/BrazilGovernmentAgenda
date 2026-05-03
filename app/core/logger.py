a1170e707d03d55fff9681aea22dccf764792964
KaioMendonca <kaiomendonca.dev@hotmail.com>
2026-05-03 20:46:09 -0300

    chore: add basic config logger.py for loggers in development or production

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,7 +1,41 @@
 import logging
+import os
+from colorlog import ColoredFormatter
+from pythonjsonlogger import jsonlogger
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s | %(levelname)s | %(message)s",
-)
-logger = logging.getLogger(__name__)
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development").lower()
+
+def get_logger(name: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+    
+    if logger.handlers:
+        return logger
+    
+    logger.setLevel(LOG_LEVEL)
+    
+    console_handler = logging.StreamHandler()
+    
+    if ENVIRONMENT == "production":
+        formatter = jsonlogger.JsonFormatter (
+            "%(asctime)s %(levelname)s %(name)s %(message)s"
+        )
+        
+    else:
+        
+        formatter = ColoredFormatter(
+            "%(log_color)s%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+            log_colors={
+                "DEBUG": "cyan",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "bold_red",
+            },
+        )
+        
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    logger.propagate = False
+    
+    return logger
